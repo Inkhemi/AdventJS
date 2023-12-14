05924aa0498377f5f42b6263118ce8bf3f8d466f
Claudio Rodriguez Herrera <claudiorh1953@gmail.com>
2023-12-14 20:57:43 -0300

    Optimized solution Reversed if statements for better readability

--- Codes/day_11.js
+++ Codes/day_11.js
@@ -1,51 +1,40 @@
 /*
 En el taller de Santa, los elfos aman los acertijos 🧠.
- Este año, han creado uno especial: un desafío para formar un palíndromo navideño.
+ Este año, han creado uno especial: un desafío para formar un palindromo navideño.
 
-Un palíndromo es una palabra que se lee igual hacia adelante y hacia atrás.
- Los elfos quieren saber si es posible formar un palíndromo haciendo, como mucho, un intercambio de letras.
+Un palindromo es una palabra que se lee igual hacia adelante y hacia atrás.
+ Los elfos quieren saber si es posible formar un palindromo haciendo, como mucho, un intercambio de letras.
 
 Crea una función getIndexsForPalindrome que reciba una cadena de caracteres y devolverá:
 
-Si ya es un palíndromo, un array vacío.
+Si ya es un palindromo, un array vacío.
 Si no es posible, null.
-Si se puede formar un palíndromo con un cambio,
+Si se puede formar un palindromo con un cambio,
  un array con las dos posiciones (índices) que se deben intercambiar para poder crearlo.
 */
 
 function getIndexsForPalindrome(word) {
-  let reverseWord = ""
-  let index = []
+  const reverseWord = word.split("").reverse().join("")
 
-  const wordLength = " ".repeat(word.length)
-
-  for (const i of [...wordLength].keys()) {
-    reverseWord = word[i] + reverseWord
+  if (word === reverseWord) {
+    return []
   }
 
-  if (word === reverseWord) return []
+  let iterable = ",".repeat(word.length)
 
-  for (const i of [...wordLength].keys()) {
-    for (const j of [...wordLength].keys()) {
-      if (i !== j) {
-        const newWord =
-          word.slice(0, i) +
-          word[j] +
-          word.slice(i + 1, j) +
-          word[i] +
-          word.slice(j + 1)
+  for (const i of [...iterable].keys()) {
+    for (const j of [...iterable].keys()) {
+      const newWord =
+        word.slice(0, i) +
+        word[j] +
+        word.slice(i + 1, j) +
+        word[i] +
+        word.slice(j + 1)
 
-        let reverseNewWord = ""
+      const reverseNewWord = newWord.split("").reverse().join("")
 
-        for (const k of [...wordLength].keys()) {
-          reverseNewWord = newWord[k] + reverseNewWord
-        }
-
-        if (newWord === reverseNewWord) {
-          index.push(i)
-          index.push(j)
-          return index
-        }
+      if (newWord === reverseNewWord) {
+        return [i, j]
       }
     }
   }
